6b3f06e1eae4652aba4ea633b168827fabc40854
unknown <shibaon@gmail.com>
2015-08-14 00:09:10 +0700

    Added imageResize filter into twig

--- vendor/svi/file/src/Bundle.php
+++ vendor/svi/file/src/Bundle.php
@@ -2,8 +2,19 @@
 
 namespace Svi\File;
 
+use Svi\Application;
+use Svi\File\Twig\ImageTwigExtension;
+
 class Bundle extends \Svi\Bundle
 {
+
+	function __construct(Application $app)
+	{
+		parent::__construct($app);
+		if ($app->getTwig()) {
+			$app->getTwig()->addExtension(new ImageTwigExtension($app));
+		}
+	}
 
 	protected function getServices()
 	{
