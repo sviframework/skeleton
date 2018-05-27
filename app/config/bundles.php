5b851c281a3cab6916376390b3c4196afe5231c0
Valery <shibaon@gmail.com>
2018-05-27 10:44:07 +0300

    Removed unnecessary requiremens

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -2,10 +2,6 @@
 return [
     \Svi\TengineBundle\Bundle::class,
     \Svi\HttpBundle\Bundle::class,
-    \Svi\OrmBundle\Bundle::class,
-    \Svi\CrudBundle\Bundle::class,
-    \Svi\MailBundle\Bundle::class,
-    \Svi\FileBundle\Bundle::class,
 
     \HelloBundle\Bundle::class,
 ];
